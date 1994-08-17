109d54f9163b311808318a570ed7ed9f1029d158
Todd C. Miller <Todd.Miller@courtesan.com>
1994-08-17 16:36:31 +0000

    added unistd.h include

--- sudo_setenv.c
+++ sudo_setenv.c
@@ -38,6 +38,10 @@ static char rcsid[] = "$Id$";
 #ifdef HAVE_MALLOC_H
 #include <malloc.h>
 #endif /* HAVE_MALLOC_H */
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif /* HAVE_UNISTD_H */
+#include <sys/types.h>
 #include <sys/param.h>
 #include <netinet/in.h>
 
