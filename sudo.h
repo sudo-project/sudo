8de40d1c3ce685e2fae294a05b03b0d6e9d506bd
Todd C. Miller <Todd.Miller@courtesan.com>
1995-03-26 06:19:48 +0000

    no more sudo_realpath() and find_path() changed params

--- sudo.h
+++ sudo.h
@@ -161,11 +161,10 @@ char *getcwd		__P((char *, size_t));
 #if !defined(HAVE_PUTENV) && !defined(HAVE_SETENV)
 int putenv		__P((const char *));
 #endif
-char *sudo_realpath	__P((const char *, char *));
 char *sudo_goodpath	__P((const char *));
 int sudo_setenv		__P((char *, char *));
 char *tgetpass		__P((char *, int));
-int find_path		__P((char *, char **, char **));
+char * find_path	__P((char *));
 void log_error		__P((int));
 void inform_user	__P((int));
 void check_user		__P((void));
