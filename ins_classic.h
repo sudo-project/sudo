d87459b87af332e791992b4f2c1cc6a664e54e87
Todd C. Miller <Todd.Miller@courtesan.com>
1995-05-02 05:35:29 +0000

    Initial revision

---
+++ ins_classic.h
@@ -0,0 +1,45 @@
+/*
+ *  CU sudo version 1.3.1
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 1, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Please send bugs, changes, problems to sudo-bugs@cs.colorado.edu
+ *
+ *  $Id$
+ */
+
+#ifndef _SUDO_INS_CLASSIC_H
+#define _SUDO_INS_CLASSIC_H
+
+/*
+ * Insults from the original sudo(8).
+ * To add insult to injury, just add to the following strings and
+ * adjust NOFINSULTS accordingly.
+ */
+
+char *insults[] = {
+    "Wrong!  You cheating scum!",
+    "No soap, honkie-lips.",
+    "Where did you learn to type?",
+    "Are you on drugs?",
+    "My pet ferret can type better than you!",
+    "You type like i drive.",
+    "Do you think like you type?",
+    "Your mind just hasn't been the same since the electro-shock, has it?"
+};
+
+#define NOFINSULTS 8 	/* number of insults */
+
+#endif /* _SUDO_INS_CLASSIC_H */
