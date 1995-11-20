7dba81c21d245515d3bff6649696c97b80abeb38
Todd C. Miller <Todd.Miller@courtesan.com>
1995-11-20 03:30:46 +0000

    DCE & KERB support

--- tgetpass.c
+++ tgetpass.c
@@ -68,7 +68,11 @@ static char rcsid[] = "$Id$";
 #  ifdef HAVE_C2_SECURITY
 #    define	_PASSWD_LEN	24
 #  else
-#    define	_PASSWD_LEN	8
+#    if defined(HAVE_DCE) || defined(HAVE_KERB4) || defined(HAVE_KERB5)
+#      define	_PASSWD_LEN	256
+#    else
+#      define	_PASSWD_LEN	8
+#    endif /* HAVE_DCE || HAVE_KERB[45] */
 #  endif /* HAVE_C2_SECURITY */
 #endif /* _PASSWD_LEN */
 
