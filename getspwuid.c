eb09cb8fa5d900123732bc2613c9ff07efed83e7
Todd C. Miller <Todd.Miller@courtesan.com>
1995-11-21 21:27:08 +0000

    now includes string.h for strdup() prototype

--- getspwuid.c
+++ getspwuid.c
@@ -37,6 +37,12 @@ static char rcsid[] = "$Id$";
 #ifdef STDC_HEADERS
 #include <stdlib.h>
 #endif /* STDC_HEADERS */
+#if defined(HAVE_MALLOC_H) && !defined(STDC_HEADERS)
+#include <malloc.h>   
+#endif /* HAVE_MALLOC_H && !STDC_HEADERS */
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif /* HAVE_STRING_H */
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif /* HAVE_UNISTD_H */
