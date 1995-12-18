6846377706b8e7a40081d3059ddbe2c5797c2f14
Todd C. Miller <Todd.Miller@courtesan.com>
1995-12-18 03:09:49 +0000

    ++version

--- getspwuid.c
+++ getspwuid.c
@@ -1,5 +1,5 @@
 /*
- *  CU sudo version 1.3.6
+ *  CU sudo version 1.3.7
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
