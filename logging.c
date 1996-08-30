d189849b447fcf2b2a52feac324b74893742a08b
Todd C. Miller <Todd.Miller@courtesan.com>
1996-08-30 00:37:04 +0000

    ++version

--- logging.c
+++ logging.c
@@ -1,5 +1,5 @@
 /*
- * CU sudo version 1.5 (based on Root Group sudo version 1.1)
+ * CU sudo version 1.5.1 (based on Root Group sudo version 1.1)
  *
  * This software comes with no waranty whatsoever, use at your own risk.
  *
--- tgetpass.c
+++ tgetpass.c
@@ -1,5 +1,5 @@
 /*
- *  CU sudo version 1.5
+ *  CU sudo version 1.5.1
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
