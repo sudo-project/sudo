09bb1a3bf7dc9747ac7ac3051c36768fe13f8d60
Todd C. Miller <Todd.Miller@courtesan.com>
1998-04-06 03:25:36 +0000

    add rcsid

--- lsearch.c
+++ lsearch.c
@@ -37,6 +37,9 @@
 #if defined(LIBC_SCCS) && !defined(lint)
 static char sccsid[] = "@(#)lsearch.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
+#ifndef lint
+static char rcsid[] = "$Id$";
+#endif /* lint */
 
 #include "config.h"
 
