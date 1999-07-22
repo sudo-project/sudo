1403ace0840a538f53d1ffa9f8e51b934e5f5b16
Todd C. Miller <Todd.Miller@courtesan.com>
1999-07-22 12:39:07 +0000

    BSD-style copyright

--- alloc.c
+++ alloc.c
@@ -1,28 +1,28 @@
 /*
- *  CU sudo version 1.6
- *  Copyright (c) 1999 Todd C. Miller <Todd.Miller@courtesan.com>
+ * Copyright (c) 1999 Todd C. Miller <Todd.Miller@courtesan.com>
+ * All rights reserved.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 1, or (at your option)
- *  any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *  Please send bugs, changes, problems to sudo-bugs@courtesan.com
- *
- *******************************************************************
- *
- *  This module contains memory allocation routines used by sudo.
- *
- *  Todd C. Miller <Todd.Miller@courtesan.com> Fri Jun  3 18:32:19 MDT 1994
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "config.h"
@@ -44,9 +44,9 @@
 #include "compat.h"
 
 #ifndef STDC_HEADERS
-#ifndef __GNUC__		/ *gcc has its own malloc */
+#if !defined(__GNUC__) && !defined(HAVE_MALLOC_H)
 extern VOID *malloc	__P((size_t));
-#endif /* __GNUC__ */
+#endif /* !__GNUC__ && !HAVE_MALLOC_H */
 #endif /* !STDC_HEADERS */
 
 extern char **Argv;		/* from sudo.c */
@@ -56,14 +56,10 @@ static const char rcsid[] = "$Sudo$";
 #endif /* lint */
 
 
-/**********************************************************************
- *
- * emalloc()
- *
- *  emalloc() calls the system malloc(3) and exits with an error if
- *  malloc(3) fails.
+/*
+ * emalloc() calls the system malloc(3) and exits with an error if
+ * malloc(3) fails.
  */
-
 VOID *
 emalloc(size)
     size_t size;
@@ -77,15 +73,11 @@ emalloc(size)
     return(ptr);
 }
 
-/**********************************************************************
- *
- * erealloc()
- *
- *  erealloc() calls the system realloc(3) and exits with an error if
- *  realloc(3) fails.  You can call erealloc() with a NULL pointer even
- *  if the system realloc(3) does not support this.
+/*
+ * erealloc() calls the system realloc(3) and exits with an error if
+ * realloc(3) fails.  You can call erealloc() with a NULL pointer even
+ * if the system realloc(3) does not support this.
  */
-
 VOID *
 erealloc(ptr, size)
     VOID *ptr;
@@ -99,14 +91,10 @@ erealloc(ptr, size)
     return(ptr);
 }
 
-/**********************************************************************
- *
- * estrdup()
- *
- *  estrdup() is like strdup(3) except that it exits with an error if
- *  malloc(3) fails.  NOTE: unlike strdup(3), estrdup(NULL) is legal.
+/*
+ * estrdup() is like strdup(3) except that it exits with an error if
+ * malloc(3) fails.  NOTE: unlike strdup(3), estrdup(NULL) is legal.
  */
-
 char *
 estrdup(src)
     const char *src;
