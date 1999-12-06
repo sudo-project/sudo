8446da40fb450135290ded41ec1aa3a02da36031
Todd C. Miller <Todd.Miller@courtesan.com>
1999-12-06 06:47:19 +0000

    Don't allow insults to be enabled if the insults[] array is empty. Otherwise there would be division by zero.

--- auth/sudo_auth.c
+++ auth/sudo_auth.c
@@ -224,9 +224,11 @@ pass_warn(fp)
     FILE *fp;
 {
 
+#ifdef INSULT
     if (def_flag(I_INSULTS))
 	(void) fprintf(fp, "%s\n", INSULT);
     else
+#endif
 	(void) fprintf(fp, "%s\n", def_str(I_BADPASS_MSG));
 }
 
