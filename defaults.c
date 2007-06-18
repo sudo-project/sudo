efd9e8806ae2e5454a071276867d4fccf127da55
Todd C. Miller <Todd.Miller@courtesan.com>
2007-06-18 15:51:35 +0000

    env_reset is now the default.
    --HG--
    branch : 1.6

--- defaults.c
+++ defaults.c
@@ -436,6 +436,7 @@ init_defaults()
 #ifdef ENV_EDITOR
     def_env_editor = TRUE;
 #endif
+    def_env_reset = TRUE;
     def_set_logname = TRUE;
 
     /* Syslog options need special care since they both strings and ints */
