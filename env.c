0d11740e5cf708a8306df1147ace00eb3b21ac17
Todd C. Miller <Todd.Miller@courtesan.com>
2007-07-29 20:10:19 +0000

    use correct length when making a copy of environ
    --HG--
    branch : 1.6

--- env.c
+++ env.c
@@ -629,7 +629,7 @@ insert_env_vars(envp, env_vars)
 	    env.env_size = evlen + 1 + 128;
 	    env.envp = emalloc2(env.env_size, sizeof(char *));
 	}
-	memcpy(env.envp, envp, evlen + 1);
+	memcpy(env.envp, envp, (evlen + 1) * sizeof(char *));
 	env.env_len = evlen;
     }
 
