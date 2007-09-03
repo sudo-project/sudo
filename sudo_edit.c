bccb884914bfdeb7ef1cabd83bdce38f24a45979
Todd C. Miller <Todd.Miller@courtesan.com>
2007-09-03 20:28:31 +0000

    Fix sudoedit of a non-existent file.  From Tilo Stritzky.
    --HG--
    branch : 1.6

--- sudo_edit.c
+++ sudo_edit.c
@@ -135,7 +135,7 @@ int sudo_edit(argc, argv, envp)
 	    }
 	}
 	set_perms(PERM_ROOT);
-	if (error || !S_ISREG(sb.st_mode)) {
+	if (error || (ofd != -1 && !S_ISREG(sb.st_mode))) {
 	    if (error)
 		warn("%s", *ap);
 	    else
