5c4840365944ae5887ea1d465fbc602471581590
Todd C. Miller <Todd.Miller@courtesan.com>
2008-11-06 00:42:37 +0000

    just use RLIM_INFINITY for RLIM_SAVED_MAX if RLIM_SAVED_MAX not defined

--- aix.c
+++ aix.c
@@ -39,7 +39,7 @@ __unused static const char rcsid[] = "$Sudo$";
 #ifdef HAVE_GETUSERATTR
 
 #ifndef RLIM_SAVED_MAX
-# define RLIM_SAVED_MAX 0x7fffffff
+# define RLIM_SAVED_MAX	RLIM_INFINITY
 #endif
 
 struct aix_limit {
