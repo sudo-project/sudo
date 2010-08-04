35305cf2f5846e9da61d67c750e9c74506a8204a
Todd C. Miller <Todd.Miller@courtesan.com>
2010-08-04 09:58:34 -0400

    Make sure we don't try to delref NULL.
    --HG--
    branch : 1.7

--- check.c
+++ check.c
@@ -163,8 +163,10 @@ check_user(validated, mode)
 	update_timestamp(timestampdir, timestampfile);
     efree(timestampdir);
     efree(timestampfile);
-    pw_delref(auth_pw);
-    auth_pw = NULL;
+    if (auth_pw != NULL) {
+	pw_delref(auth_pw);
+	auth_pw = NULL;
+    }
 }
 
 /*
