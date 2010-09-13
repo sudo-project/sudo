157624f224346fea6f7c2b1962bbb7c6e760c758
Todd C. Miller <Todd.Miller@courtesan.com>
2010-09-13 11:48:51 -0400

    Use __sun for consistency with the rest of the sources.
    --HG--
    branch : 1.7

--- bsm_audit.c
+++ bsm_audit.c
@@ -35,7 +35,7 @@
  * Solaris auditon() returns EINVAL if BSM audit not configured.
  * OpenBSM returns ENOSYS for unimplemented options.
  */
-#ifdef __sun__
+#ifdef __sun
 # define AUDIT_NOT_CONFIGURED	EINVAL
 #else
 # define AUDIT_NOT_CONFIGURED	ENOSYS
