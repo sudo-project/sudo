b679e031951b7f825547e2b258bf697e6e80304c
Todd C. Miller <Todd.Miller@courtesan.com>
2011-01-07 13:19:32 -0500

    Add support for regress tests using testsudoers

---
+++ plugins/sudoers/regress/testsudoers/test1.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Test for NULL dereference with "sudo -g group" when the sudoers rule
+# has no runas user or group listed.
+# This is RedHat bug Bug 667103.
+#
+
+WANT=${SRCDIR-.}/regress/testsudoers/test1.ok
+GOT=test1.out
+./testsudoers -g wheel root id > $GOT <<EOF
+root ALL = ALL
+EOF
+
+# Check results
+if cmp $WANT $GOT >/dev/null; then
+    echo "testsudoers 1: OK"
+else
+    echo "testsudoers 1: FAILED"
+    diff $WANT $GOT
+fi
