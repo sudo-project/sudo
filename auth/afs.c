46d352b4e309b26669f837b15a71b582d69d905c
Todd C. Miller <Todd.Miller@courtesan.com>
2011-02-17 11:58:26 -0500

    Move afs includes to be before sudo ones
    --HG--
    branch : 1.7

--- auth/afs.c
+++ auth/afs.c
@@ -43,11 +43,11 @@
 #endif /* HAVE_UNISTD_H */
 #include <pwd.h>
 
-#include "sudo.h"
-#include "sudo_auth.h"
-
 #include <afs/stds.h>
 #include <afs/kautils.h>
+
+#include "sudo.h"
+#include "sudo_auth.h"
 
 int
 afs_verify(pw, pass, auth)
