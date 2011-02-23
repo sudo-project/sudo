a64bd36b339d8b40021c54a411e5ff25d08f0111
Todd C. Miller <Todd.Miller@courtesan.com>
2011-02-23 15:26:13 -0500

    convert test2 to use testsudoers

--- plugins/sudoers/regress/testsudoers/test2.sh
+++ plugins/sudoers/regress/testsudoers/test2.sh
@@ -4,9 +4,9 @@
 # See http://www.sudo.ws/bugs/show_bug.cgi?id=437
 #
 
-WANT=${SRCDIR-.}/regress/visudo/test1.ok
-GOT=vs_test1.out
-./visudo -c -f- > $GOT <<EOF
+WANT=${SRCDIR-.}/regress/testsudoers/test2.out
+GOT=ts_test2.out
+./testsudoers -d > $GOT <<EOF
 "%:C/non UNIX 0 c" ALL=(ALL) ALL
 "%:C/non\'UNIX\'1 c" ALL=(ALL) ALL
 "%:C/non\"UNIX\"0 c" ALL=(ALL) ALL
@@ -16,8 +16,8 @@ EOF
 
 # Check results
 if cmp $WANT $GOT >/dev/null; then
-    echo "visudo 2: OK"
+    echo "testsudoers 2: OK"
 else
-    echo "visudo 2: FAILED"
+    echo "testsudoers 2: FAILED"
     diff $WANT $GOT
 fi
