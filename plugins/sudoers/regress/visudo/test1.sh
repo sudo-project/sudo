c55b5c9e1858cbe3c64a87b30e0a50ae3d715d28
Todd C. Miller <Todd.Miller@courtesan.com>
2011-10-26 10:20:32 -0400

    Add regress test for bugzilla 519

---
+++ plugins/sudoers/regress/visudo/test1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Sudo Bug 519:
+# Visudo in strict mode reports "parse error" even if there is no error
+#
+
+./visudo -csf - <<EOF
+User_Alias FOO = nobody
+FOO ALL=(ALL) NOPASSWD: ALL
+EOF
