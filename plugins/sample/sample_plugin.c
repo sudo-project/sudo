0b1baf07ec9c0291826ad5f68696cfd7040683a0
Todd C. Miller <Todd.Miller@courtesan.com>
2012-03-15 09:02:19 -0400

    Add explicit NULL entries for init_session, register_hooks and deregister_hooks with appropriate comments.

--- plugins/sample/sample_plugin.c
+++ plugins/sample/sample_plugin.c
@@ -488,7 +488,10 @@ struct policy_plugin sample_policy = {
     policy_check,
     policy_list,
     NULL, /* validate */
-    NULL /* invalidate */
+    NULL, /* invalidate */
+    NULL, /* init_session */
+    NULL, /* register_hooks */
+    NULL /* deregister_hooks */
 };
 
 /*
