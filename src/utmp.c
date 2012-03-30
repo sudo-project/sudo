5eb61122a6c08afecf8fadd5d495e68deead697f
Todd C. Miller <Todd.Miller@courtesan.com>
2012-03-30 07:55:49 -0400

    Fix compat setutxent and endutxent macros for systems with setutent() but not setutxent().  From Gustavo Zacarias

--- src/utmp.c
+++ src/utmp.c
@@ -63,8 +63,8 @@
 #if !defined(HAVE_GETUTXID) && defined(HAVE_GETUTID)
 # define getutxline(u)	getutline(u)
 # define pututxline(u)	pututline(u)
-# define setutxent	setutent(u)
-# define endutxent	endutent(u)
+# define setutxent(u)	setutent(u)
+# define endutxent(u)	endutent(u)
 #endif /* !HAVE_GETUTXID && HAVE_GETUTID */
 
 #ifdef HAVE_GETUTXID
