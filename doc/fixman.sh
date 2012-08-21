8f3232ee69fcc0d977a182963ab01acf0af3d571
Todd C. Miller <Todd.Miller@courtesan.com>
2012-08-21 13:01:43 -0400

    Add COMMAND EXECUTION section that describes how sudo runs the command, the extra sudo processes and signal handling.

--- doc/fixman.sh
+++ doc/fixman.sh
@@ -42,6 +42,12 @@ case "$OUTFILE" in
 		/^\\fB\\-c\\fR \\fIclass\\fR$/,/^\.TP 12n$/ {
 			/^\.PD$/!d
 		}
+		/^login_cap(3),$/d
+		/^BSD login class$/ {
+			N
+			N
+			/^BSD login class\n\.TP 4n\n\\fBo\\fR$/d
+		}
 	EOF
 	fi
 
@@ -51,6 +57,25 @@ case "$OUTFILE" in
 		/^\[\\fB\\-[rt]\\fR\\ \\fI[rt][oy][lp]e\\fR/d
 		/^\\fB\\-[rt]\\fR \\fI[rt][oy][lp]e\\fR$/,/^\.TP 12n$/ {
 			/^\.PD$/!d
+		}
+		/^SELinux role and type$/ {
+			N
+			N
+			/^SELinux role and type\n\.TP 4n\n\\fBo\\fR$/d
+		}
+	EOF
+	fi
+
+	# Solaris privileges
+	if [ X"$PSMAN" != X"1" ]; then
+	cat >>"$OUTFILE" <<-'EOF'
+		/^Solaris project$/ {
+			N
+			N
+			N
+			N
+			N
+			/^Solaris project\n\.TP 4n\n\\fBo\\fR\nSolaris privileges\n\.TP 4n\n\\fBo\\fR$/d
 		}
 	EOF
 	fi
