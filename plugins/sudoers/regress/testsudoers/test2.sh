cca915e39bddb19cd5828d6bfe2c2187865a655a
Todd C. Miller <Todd.Miller@courtesan.com>
2012-11-25 08:12:55 -0500

    Bash doesn't let you set UID to use MYUID instead.

--- plugins/sudoers/regress/testsudoers/test2.sh
+++ plugins/sudoers/regress/testsudoers/test2.sh
@@ -4,8 +4,8 @@
 #
 
 USER=`\ls -l $TESTDIR/test2.inc | awk '{print $3}'`
-UID=`id -u $USER`
+MYUID=`id -u $USER`
 exec 2>&1
-./testsudoers -U $UID root id <<EOF
+./testsudoers -U $MYUID root id <<EOF
 #include $TESTDIR/test2.inc
 EOF
--- plugins/sudoers/regress/testsudoers/test5.sh
+++ plugins/sudoers/regress/testsudoers/test5.sh
@@ -10,18 +10,18 @@ root ALL = ALL
 EOF
 
 USER=`\ls -l $TESTFILE | awk '{print $3}'`
-UID=`id -u $USER`
+MYUID=`id -u $USER`
 exec 2>&1
 
 # Test world writable
 chmod 666 $TESTFILE
-./testsudoers -U $UID root id <<EOF
+./testsudoers -U $MYUID root id <<EOF
 #include $TESTFILE
 EOF
 
 # Test group writable
 chmod 664 $TESTFILE
-./testsudoers -U $UID -G 1 root id <<EOF
+./testsudoers -U $MYUID -G 1 root id <<EOF
 #include $TESTFILE
 EOF
 
