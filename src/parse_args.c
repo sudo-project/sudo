a5b64a1d2e11e522cbf6486339a61cbc5f34dc14
Todd C. Miller <Todd.Miller@courtesan.com>
2013-08-16 09:41:46 -0600

    Use lower card for the long option arguments to match the manual. This is inconsistent with GNU but it is better to match the sudo documentation.

--- src/parse_args.c
+++ src/parse_args.c
@@ -614,29 +614,29 @@ help(void)
     lbuf_append(&lbuf, "  -A, --askpass               %s\n",
 	_("use a helper program for password prompting"));
 #ifdef HAVE_BSD_AUTH_H
-    lbuf_append(&lbuf, "  -a, --auth-type=TYPE   %s\n",
+    lbuf_append(&lbuf, "  -a, --auth-type=type   %s\n",
 	_("use specified BSD authentication type"));
 #endif
     lbuf_append(&lbuf, "  -b, --background            %s\n",
 	_("run command in the background"));
-    lbuf_append(&lbuf, "  -C, --close-from=NUM        %s\n",
-	_("close all file descriptors >= NUM"));
+    lbuf_append(&lbuf, "  -C, --close-from=num        %s\n",
+	_("close all file descriptors >= num"));
 #ifdef HAVE_LOGIN_CAP_H
-    lbuf_append(&lbuf, "  -c, --login-class=CLASS     %s\n",
+    lbuf_append(&lbuf, "  -c, --login-class=class     %s\n",
 	_("run command with the specified BSD login class"));
 #endif
     lbuf_append(&lbuf, "  -E, --preserve-env          %s\n",
 	_("preserve user environment when running command"));
     lbuf_append(&lbuf, "  -e, --edit                  %s\n",
 	_("edit files instead of running a command"));
-    lbuf_append(&lbuf, "  -g, --group=GROUP           %s\n",
+    lbuf_append(&lbuf, "  -g, --group=group           %s\n",
 	_("run command as the specified group name or ID"));
     lbuf_append(&lbuf, "  -H, --set-home              %s\n",
 	_("set HOME variable to target user's home dir"));
     lbuf_append(&lbuf, "  -h, --help                  %s\n",
 	_("display help message and exit"));
-    lbuf_append(&lbuf, "  -h, --host=HOST             %s\n",
-	_("run command on HOST (if supported by plugin)"));
+    lbuf_append(&lbuf, "  -h, --host=host             %s\n",
+	_("run command on host (if supported by plugin)"));
     lbuf_append(&lbuf, "  -i, --login                 %s\n",
 	_("run login shell as the target user; a command may also be specified"));
     lbuf_append(&lbuf, "  -K, --remove-timestamp      %s\n",
@@ -649,10 +649,10 @@ help(void)
 	_("non-interactive mode, no prompts are used"));
     lbuf_append(&lbuf, "  -P, --preserve-groups       %s\n",
 	_("preserve group vector instead of setting to target's"));
-    lbuf_append(&lbuf, "  -p, --prompt=PROMPT         %s\n",
+    lbuf_append(&lbuf, "  -p, --prompt=prompt         %s\n",
 	_("use the specified password prompt"));
 #ifdef HAVE_SELINUX
-    lbuf_append(&lbuf, "  -r, --role=ROLE             %s\n",
+    lbuf_append(&lbuf, "  -r, --role=role             %s\n",
 	_("create SELinux security context with specified role"));
 #endif
     lbuf_append(&lbuf, "  -S, --stdin                 %s\n",
@@ -660,12 +660,12 @@ help(void)
     lbuf_append(&lbuf, "  -s, --shell                 %s\n",
 	_("run shell as the target user; a command may also be specified"));
 #ifdef HAVE_SELINUX
-    lbuf_append(&lbuf, "  -t, --type=TYPE             %s\n",
+    lbuf_append(&lbuf, "  -t, --type=type             %s\n",
 	_("create SELinux security context with specified type"));
 #endif
-    lbuf_append(&lbuf, "  -U, --other-user=USER       %s\n",
-	_("in list mode, display privileges for USER"));
-    lbuf_append(&lbuf, "  -u, --user=USER             %s\n",
+    lbuf_append(&lbuf, "  -U, --other-user=user       %s\n",
+	_("in list mode, display privileges for user"));
+    lbuf_append(&lbuf, "  -u, --user=user             %s\n",
 	_("run command (or edit file) as specified user name or ID"));
     lbuf_append(&lbuf, "  -V, --version               %s\n",
 	_("display version information and exit"));
