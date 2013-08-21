40b24c5da0fc0e57fcfc059da272554aca312b3b
Todd C. Miller <Todd.Miller@courtesan.com>
2013-08-21 10:09:04 -0600

    Add configure checks for the exec functions we will dummy out.  This is only really needed on Mac OS X when symbol interposition is being performed but won't hurt elsewhere.

--- src/sudo_noexec.c
+++ src/sudo_noexec.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2005, 2010-2012 Todd C. Miller <Todd.Miller@courtesan.com>
+ * Copyright (c) 2004-2005, 2010-2013 Todd C. Miller <Todd.Miller@courtesan.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -39,22 +39,22 @@ typedef struct interpose_s {
     void *orig_func;
 } interpose_t;
 
-# define FN_NAME(fn)	my_ ## fn
+# define FN_NAME(fn)	dummy_ ## fn
 # define INTERPOSE(fn) \
-    __attribute__((used)) static const interpose_t _interpose_ ## fn \
-    __attribute__((section("__DATA,__interpose"))) = \
-	{ (void *)my_ ## fn, (void *)fn };
+    __attribute__((__used__)) static const interpose_t interpose_ ## fn \
+    __attribute__((__section__("__DATA,__interpose"))) = \
+	{ (void *)dummy_ ## fn, (void *)fn };
 #else
 # define FN_NAME(fn)	fn
 # define INTERPOSE(fn)
 #endif
 
 /*
- * Dummy versions of the execve() family of syscalls.  We don't need
- * to stub out all of them, just the ones that correspond to actual
- * system calls (which varies by OS).  Note that it is still possible
- * to access the real syscalls via the syscall() interface but very
- * few programs actually do that.
+ * Dummy versions of the exec(3) family of syscalls.  It is not enough
+ * to just dummy out execve(2) since some C libraries use direct syscalls
+ * for the other functions instead of calling execve(2).  Note that it is
+ * still possible to access the real syscalls via the syscall(2) interface
+ * but very few programs actually do that.
  */
 
 #define DUMMY_BODY				\
@@ -87,51 +87,118 @@ FN_NAME(fn)(t1 a1, t2 a2, ...)			\
 DUMMY_BODY					\
 INTERPOSE(fn)
 
+/*
+ * Standard exec(3) family of functions.
+ */
 DUMMY_VA(execl, const char *, const char *)
 DUMMY_VA(execle, const char *, const char *)
 DUMMY_VA(execlp, const char *, const char *)
 DUMMY2(execv, const char *, char * const *)
 DUMMY2(execvp, const char *, char * const *)
-DUMMY3(execvP, const char *, const char *, char * const *)
 DUMMY3(execve, const char *, char * const *, char * const *)
 
-#ifndef __APPLE__
 /*
- * On Mac OS X we can only define functions that actually
- * exist on the system due to how interposition is done.
+ * Private versions of the above.
  */
-DUMMY3(exect, const char *, char * const *, char * const *)
-DUMMY3(execvpe, const char *, char * const *, char * const *)
-DUMMY3(fexecve, int , char * const *, char * const *)
+#ifdef HAVE__EXECL
 DUMMY_VA(_execl, const char *, const char *)
-DUMMY_VA(_execle, const char *, const char *)
-DUMMY_VA(_execlp, const char *, const char *)
-DUMMY3(_exect, const char *, char * const *, char * const *)
-DUMMY2(_execv, const char *, char * const *)
-DUMMY2(_execvp, const char *, char * const *)
-DUMMY3(_execvP, const char *, const char *, char * const *)
-DUMMY3(_execve, const char *, char * const *, char * const *)
-DUMMY3(_execvpe, const char *, char * const *, char * const *)
-DUMMY3(_fexecve, int , char * const *, char * const *)
+#endif
+#ifdef HAVE___EXECL
 DUMMY_VA(__execl, const char *, const char *)
+#endif
+#ifdef HAVE__EXECLE
+DUMMY_VA(_execle, const char *, const char *)
+#endif
+#ifdef HAVE___EXECLE
 DUMMY_VA(__execle, const char *, const char *)
+#endif
+#ifdef HAVE__EXECLP
+DUMMY_VA(_execlp, const char *, const char *)
+#endif
+#ifdef HAVE___EXECLP
 DUMMY_VA(__execlp, const char *, const char *)
-DUMMY3(__exect, const char *, char * const *, char * const *)
+#endif
+#ifdef HAVE__EXECV
+DUMMY2(_execv, const char *, char * const *)
+#endif
+#ifdef HAVE___EXECV
 DUMMY2(__execv, const char *, char * const *)
+#endif
+#ifdef HAVE__EXECVP
+DUMMY2(_execvp, const char *, char * const *)
+#endif
+#ifdef HAVE___EXECVP
 DUMMY2(__execvp, const char *, char * const *)
-DUMMY3(__execvP, const char *, const char *, char * const *)
+#endif
+#ifdef HAVE__EXECVE
+DUMMY3(_execve, const char *, char * const *, char * const *)
+#endif
+#ifdef HAVE___EXECVE
 DUMMY3(__execve, const char *, char * const *, char * const *)
-DUMMY3(__execvpe, const char *, char * const *, char * const *)
-DUMMY3(__fexecve, int , char * const *, char * const *)
-#endif /* __APPLE__ */
+#endif
 
-#ifdef HAVE_SPAWN_H
+/*
+ * Non-standard exec functions and corresponding private versions.
+ */
+#ifdef HAVE_EXECVP
+DUMMY3(execvP, const char *, const char *, char * const *)
+#endif
+#ifdef HAVE__EXECVP
+DUMMY3(_execvP, const char *, const char *, char * const *)
+#endif
+#ifdef HAVE___EXECVP
+DUMMY3(__execvP, const char *, const char *, char * const *)
+#endif
+
+#ifdef HAVE_EXECVPE
+DUMMY3(execvpe, const char *, char * const *, char * const *)
+#endif
+#ifdef HAVE__EXECVPE
+DUMMY3(_execvpe, const char *, char * const *, char * const *)
+#endif
+#ifdef HAVE___EXECVPE
+DUMMY3(__execvpe, const char *, char * const *, char * const *)
+#endif
+
+#ifdef HAVE_EXECT
+DUMMY3(exect, const char *, char * const *, char * const *)
+#endif
+#ifdef HAVE__EXECT
+DUMMY3(_exect, const char *, char * const *, char * const *)
+#endif
+#ifdef HAVE___EXECT
+DUMMY3(__exect, const char *, char * const *, char * const *)
+#endif
+
+#ifdef HAVE_FEXECVE
+DUMMY3(fexecve, int , char * const *, char * const *)
+#endif
+#ifdef HAVE__FEXECVE
+DUMMY3(_fexecve, int , char * const *, char * const *)
+#endif
+#ifdef HAVE___FEXECVE
+DUMMY3(__fexecve, int , char * const *, char * const *)
+#endif
+
+/*
+ * posix_spawn, posix_spawnp and any private versions.
+ */
+#ifdef HAVE_POSIX_SPAWN
 DUMMY6(posix_spawn, pid_t *, const char *, const posix_spawn_file_actions_t *, const posix_spawnattr_t *, char * const *, char * const *)
-DUMMY6(posix_spawnp, pid_t *, const char *, const posix_spawn_file_actions_t *, const posix_spawnattr_t *, char * const *, char * const *)
-# ifndef __APPLE__
+#endif
+#ifdef HAVE__POSIX_SPAWN
 DUMMY6(_posix_spawn, pid_t *, const char *, const posix_spawn_file_actions_t *, const posix_spawnattr_t *, char * const *, char * const *)
-DUMMY6(_posix_spawnp, pid_t *, const char *, const posix_spawn_file_actions_t *, const posix_spawnattr_t *, char * const *, char * const *)
+#endif
+#ifdef HAVE___POSIX_SPAWN
 DUMMY6(__posix_spawn, pid_t *, const char *, const posix_spawn_file_actions_t *, const posix_spawnattr_t *, char * const *, char * const *)
+#endif
+
+#ifdef HAVE_POSIX_SPAWNP
+DUMMY6(posix_spawnp, pid_t *, const char *, const posix_spawn_file_actions_t *, const posix_spawnattr_t *, char * const *, char * const *)
+#endif
+#ifdef HAVE_POSIX__SPAWNP
+DUMMY6(_posix_spawnp, pid_t *, const char *, const posix_spawn_file_actions_t *, const posix_spawnattr_t *, char * const *, char * const *)
+#endif
+#ifdef HAVE_POSIX___SPAWNP
 DUMMY6(__posix_spawnp, pid_t *, const char *, const posix_spawn_file_actions_t *, const posix_spawnattr_t *, char * const *, char * const *)
-# endif /* __APPLE__ */
-#endif /* HAVE_SPAWN_H */
+#endif
