12fc51060e5a1e03c5c5b685014c51bba25f00fa
Todd C. Miller <Todd.Miller@courtesan.com>
2014-01-08 16:45:06 -0700

    When writing length to timing file, use %u not %d as it is unsigned.

--- plugins/sudoers/iolog.c
+++ plugins/sudoers/iolog.c
@@ -705,11 +705,11 @@ sudoers_io_log(const char *buf, unsigned int len, int idx)
     timevalsub(&delay, &last_time);
 #ifdef HAVE_ZLIB_H
     if (iolog_compress)
-	gzprintf(io_log_files[IOFD_TIMING].fd.g, "%d %f %d\n", idx,
+	gzprintf(io_log_files[IOFD_TIMING].fd.g, "%d %f %u\n", idx,
 	    delay.tv_sec + ((double)delay.tv_usec / 1000000), len);
     else
 #endif
-	fprintf(io_log_files[IOFD_TIMING].fd.f, "%d %f %d\n", idx,
+	fprintf(io_log_files[IOFD_TIMING].fd.f, "%d %f %u\n", idx,
 	    delay.tv_sec + ((double)delay.tv_usec / 1000000), len);
     last_time.tv_sec = now.tv_sec;
     last_time.tv_usec = now.tv_usec;
