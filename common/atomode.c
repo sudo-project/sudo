5a636f2ea12d8bb3481671e94c9ae282e6633163
Todd C. Miller <Todd.Miller@courtesan.com>
2014-01-30 13:06:29 -0700

    Zero out errstr when there is no error; fixes bug #632

--- common/atomode.c
+++ common/atomode.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Todd C. Miller <Todd.Miller@courtesan.com>
+ * Copyright (c) 2013-2014 Todd C. Miller <Todd.Miller@courtesan.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -62,5 +62,7 @@ atomode(const char *cp, const char **errstr)
 	errno = ERANGE;
 	debug_return_int(0);
     }
+    if (errstr != NULL)
+	*errstr = NULL;
     debug_return_int((int)lval);
 }
