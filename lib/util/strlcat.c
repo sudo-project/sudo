5647a1ac469966263493291e9ec4dae755ca08e1
Todd C. Miller <Todd.Miller@courtesan.com>
2015-03-02 14:42:17 -0700

    Make comment match code.

--- lib/util/strlcat.c
+++ lib/util/strlcat.c
@@ -31,7 +31,7 @@
  * full size of dst, not space left).  At most dsize-1 characters
  * will be copied.  Always NUL terminates (unless dsize <= strlen(dst)).
  * Returns strlen(src) + MIN(dsize, strlen(initial dst)).
- * If retval >= siz, truncation occurred.
+ * If retval >= dsize, truncation occurred.
  */
 size_t
 sudo_strlcat(char *dst, const char *src, size_t dsize)
