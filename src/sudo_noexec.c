9dbd45ff66370556a8b198a6b86b9b33ee03751c
Todd C. Miller <Todd.Miller@courtesan.com>
2016-10-10 08:33:57 -0600

    add missing guard around wordexp()

--- src/sudo_noexec.c
+++ src/sudo_noexec.c
@@ -164,6 +164,7 @@ FN_NAME(popen)(const char *c, const char *t)
 }
 INTERPOSE(popen)
 
+#ifdef HAVE_WORDEXP
 /*
  * We can't use a wrapper for wordexp(3) since we still want to call
  * the real wordexp(3) but with WRDE_NOCMD added to the flags argument.
@@ -211,6 +212,7 @@ FN_NAME(wordexp)(const char *words, wordexp_t *we, int flags)
 #endif /* HAVE___INTERPOSE */
 }
 INTERPOSE(wordexp)
+#endif /* HAVE_WORDEXP */
 
 /*
  * On Linux we can use a seccomp() filter to disable exec.
