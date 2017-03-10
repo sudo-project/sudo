453360666ca30ba57badbbf0ba7e68c49d89f91b
Todd C. Miller <Todd.Miller@courtesan.com>
2017-03-10 11:49:07 -0700

    Use HAVE_STDBOOL_H to detect systems w/o stdbool.h. Bug #778

--- plugins/sudoers/rcstr.c
+++ plugins/sudoers/rcstr.c
@@ -20,7 +20,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#ifdef HAVE_STDBOOL_H
+# include <stdbool.h>
+#else
+# include "compat/stdbool.h"
+#endif
 #ifdef HAVE_STRING_H
 # include <string.h>
 #endif /* HAVE_STRING_H */
