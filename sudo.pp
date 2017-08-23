0d31a1c3024032e9c890c02161385c524a636f9c
Todd C. Miller <Todd.Miller@courtesan.com>
2017-08-23 11:54:13 -0600

    Fix path to LICENSE and NEWS files that get used in the installer. Previously, the installed versions were used instead of the ones in the destdir.

--- sudo.pp
+++ sudo.pp
@@ -272,9 +272,9 @@ still allow people to get their work done."
 %if [macos]
 	pp_macos_pkg_type=flat
 	pp_macos_bundle_id=ws.sudo.pkg.sudo
-	pp_macos_pkg_license=$docdir/LICENSE
+	pp_macos_pkg_license=${pp_destdir}$docdir/LICENSE
 	pp_macos_pkg_readme=${pp_wrkdir}/ReadMe.txt
-	perl -pe 'last if (/^What/i && $seen++)' $docdir/NEWS > ${pp_wrkdir}/ReadMe.txt
+	perl -pe 'last if (/^What/i && $seen++)' ${pp_destdir}$docdir/NEWS > ${pp_wrkdir}/ReadMe.txt
 %endif
 
 %if X"$aix_freeware" = X"true"
