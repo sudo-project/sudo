7c0019d2a50e47678f2d1051150217471f61aa34
Todd C. Miller <Todd.Miller@sudo.ws>
2018-11-24 08:34:03 -0700

    Revert changes to give arguments to the .Bx macro. This is intended for things like .Bx 4.3 to generate "4.3BSD" so the argument ends up before the BSD, not after.  Just go back to using "BSD authentication" and "BSD login classes" so fixmdoc.sh can operate correctly.  Bug #861

--- doc/fixmdoc.sh
+++ doc/fixmdoc.sh
@@ -35,7 +35,7 @@ case "$OUTFILE" in
 	if [ X"$BAMAN" != X"1" ]; then
 		BA_FLAG='/^.*\n\.Op Fl a Ar type/{;N;/^.*\n\.Ek$/d;};'
 		cat >>"$OUTFILE" <<-'EOF'
-			/^\.It Fl a Ar type/,/\.Bx authentication \.$/d
+			/^\.It Fl a Ar type/,/BSD authentication\.$/d
 		EOF
 	fi
 
@@ -44,11 +44,11 @@ case "$OUTFILE" in
 	if [ X"$LCMAN" != X"1" ]; then
 		LC_FLAG='/^.*\n\.Op Fl c Ar class/{;N;/^.*\n\.Ek$/d;};'
 		cat >>"$OUTFILE" <<-'EOF'
-			/^\.It Fl c Ar class/,/\.Bx login classes \.$/d
+			/^\.It Fl c Ar class/,/BSD login classes\.$/d
 			/^\.Xr login_cap 3 ,$/d
-			/^\.Bx login class$/ {
+			/^BSD login class$/ {
 				N
-				/^\.Bx login class\n\.It$/d
+				/^BSD login class\n\.It$/d
 			}
 		EOF
 	fi
@@ -110,7 +110,7 @@ case "$OUTFILE" in
 	# BSD login class
 	if [ X"$LCMAN" != X"1" ]; then
 		cat >>"$OUTFILE" <<-'EOF'
-			/^\.No On Bx systems/,/\.$/d
+			/^On BSD systems/,/\.$/d
 			/^\.It use_loginclass$/,/^by default\./d
 		EOF
 	fi
