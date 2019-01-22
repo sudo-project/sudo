ecc9c366e469988c736629cbe88348c40dcfa31a
Todd C. Miller <Todd.Miller@sudo.ws>
2019-01-22 06:41:16 -0700

    Fix listpw=never and verifypw=never.  Bug #869

--- plugins/sudoers/parse.c
+++ plugins/sudoers/parse.c
@@ -60,7 +60,7 @@ sudoers_lookup_pseudo(struct sudo_nss_list *snl, struct passwd *pw,
     debug_decl(sudoers_lookup_pseudo, SUDOERS_DEBUG_PARSER)
 
     pwcheck = (pwflag == -1) ? never : sudo_defs_table[pwflag].sd_un.tuple;
-    nopass = (pwcheck == all) ? true : false;
+    nopass = (pwcheck == never) ? true : false;
 
     if (list_pw == NULL)
 	SET(validated, FLAG_NO_CHECK);
