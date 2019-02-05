4f74fe3a8e7e5796169ad14426ae22d5e38d283a
Todd C. Miller <Todd.Miller@sudo.ws>
2019-02-05 14:29:39 -0700

    Use correct debug_decl() names.

--- plugins/sudoers/cvtsudoers_pwutil.c
+++ plugins/sudoers/cvtsudoers_pwutil.c
@@ -86,7 +86,7 @@ cvtsudoers_make_pwitem(uid_t uid, const char *name)
     struct cache_item_pw *pwitem;
     struct passwd pw, *newpw;
     struct sudoers_string *s = NULL;
-    debug_decl(sudo_make_pwitem, SUDOERS_DEBUG_NSS)
+    debug_decl(cvtsudoers_make_pwitem, SUDOERS_DEBUG_NSS)
 
     /* Look up name or uid in filter list. */
     if (name != NULL) {
@@ -193,7 +193,7 @@ cvtsudoers_make_gritem(gid_t gid, const char *name)
     struct cache_item_gr *gritem;
     struct group gr, *newgr;
     struct sudoers_string *s = NULL;
-    debug_decl(sudo_make_gritem, SUDOERS_DEBUG_NSS)
+    debug_decl(cvtsudoers_make_gritem, SUDOERS_DEBUG_NSS)
 
     /* Look up name or gid in filter list. */
     if (name != NULL) {
@@ -305,7 +305,7 @@ cvtsudoers_make_gidlist_item(const struct passwd *pw, char * const *unused1,
     struct gid_list *gidlist;
     GETGROUPS_T *gids = NULL;
     int i, ngids = 0;
-    debug_decl(sudo_make_gidlist_item, SUDOERS_DEBUG_NSS)
+    debug_decl(cvtsudoers_make_gidlist_item, SUDOERS_DEBUG_NSS)
 
     /*
      * There's only a single gid list.
@@ -402,7 +402,7 @@ cvtsudoers_make_grlist_item(const struct passwd *pw, char * const *unused1)
     struct sudoers_string *s;
     struct group_list *grlist;
     int groupname_len;
-    debug_decl(sudo_make_grlist_item, SUDOERS_DEBUG_NSS)
+    debug_decl(cvtsudoers_make_grlist_item, SUDOERS_DEBUG_NSS)
 
     /*
      * There's only a single group list.
