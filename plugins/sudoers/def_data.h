40f470c451914c68bf613d79c8d5b7d1c9b91ce5
Todd C. Miller <Todd.Miller@sudo.ws>
2019-12-26 14:10:47 -0700

    Regenerate def_data.h, I_RUNAS_CHECK_SHELL should be 118, not 123. From Zhang Xiao
    --HG--
    branch : 1.8

--- plugins/sudoers/def_data.h
+++ plugins/sudoers/def_data.h
@@ -234,7 +234,7 @@
 #define def_log_denied          (sudo_defs_table[I_LOG_DENIED].sd_un.flag)
 #define I_RUNAS_ALLOW_UNKNOWN_ID 117
 #define def_runas_allow_unknown_id (sudo_defs_table[I_RUNAS_ALLOW_UNKNOWN_ID].sd_un.flag)
-#define I_RUNAS_CHECK_SHELL     123
+#define I_RUNAS_CHECK_SHELL     118
 #define def_runas_check_shell   (sudo_defs_table[I_RUNAS_CHECK_SHELL].sd_un.flag)
 
 enum def_tuple {
