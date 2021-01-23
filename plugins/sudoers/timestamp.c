bf4157ca34edf15766a4d84971ddfb81137a4741
Todd C. Miller <Todd.Miller@sudo.ws>
2021-01-23 08:44:00 -0700

    Fix the memset offset when converting a v1 timestamp to TS_LOCKEXCL. We want to zero the struct starting at flags, not type (which was just set). Found by Qualys.
    --HG--
    branch : 1.8

--- plugins/sudoers/timestamp.c
+++ plugins/sudoers/timestamp.c
@@ -652,8 +652,8 @@ timestamp_lock(void *vcookie, struct passwd *pw)
     } else if (entry.type != TS_LOCKEXCL) {
 	/* Old sudo record, convert it to TS_LOCKEXCL. */
 	entry.type = TS_LOCKEXCL;
-	memset((char *)&entry + offsetof(struct timestamp_entry, type), 0,
-	    nread - offsetof(struct timestamp_entry, type));
+	memset((char *)&entry + offsetof(struct timestamp_entry, flags), 0,
+	    nread - offsetof(struct timestamp_entry, flags));
 	if (ts_write(cookie->fd, cookie->fname, &entry, 0) == -1)
 	    debug_return_bool(false);
     }
