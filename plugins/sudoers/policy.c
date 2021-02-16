c09169e812aac4f54801b6ca2b565fff52ecd518
Todd C. Miller <Todd.Miller@sudo.ws>
2021-02-16 08:26:49 -0700

    No need for sudoers_cleanup() in sudoers_policy_invalidate(). The sudoers close() function is now called even for "sudo -k". Also no need to set user_cmnd, it is not used in this code path.

--- plugins/sudoers/policy.c
+++ plugins/sudoers/policy.c
@@ -1066,10 +1066,7 @@ sudoers_policy_invalidate(int unlinkit)
 {
     debug_decl(sudoers_policy_invalidate, SUDOERS_DEBUG_PLUGIN);
 
-    user_cmnd = "kill";
-    /* XXX - plugin API should support a return value for fatal errors. */
     timestamp_remove(unlinkit);
-    sudoers_cleanup();
 
     debug_return;
 }
