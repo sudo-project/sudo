7c8746bc7019cb146c24e89764c9b2e1b2a2d386
Todd C. Miller <Todd.Miller@sudo.ws>
2021-11-09 12:57:25 -0700

    Document resource limit support in command_info[] and Bump plugin API minor. This is supported beginning with sudo 1.9.9 and plugin API 1.17.

--- src/exec.c
+++ src/exec.c
@@ -314,7 +314,7 @@ sudo_terminated(struct command_status *cstat)
     debug_return_bool(false);
 }
 
-#if SUDO_API_VERSION != SUDO_API_MKVERSION(1, 17)
+#if SUDO_API_VERSION != SUDO_API_MKVERSION(1, 18)
 # error "Update sudo_needs_pty() after changing the plugin API"
 #endif
 static bool
