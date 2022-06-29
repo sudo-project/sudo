50813f8160300b2ab060726a08c5e5c1196915cd
Todd C. Miller <Todd.Miller@sudo.ws>
2022-06-29 16:41:11 -0600

    Quiet another -Wwrite-strings warning.

--- lib/util/strsignal.c
+++ lib/util/strsignal.c
@@ -47,6 +47,6 @@ sudo_strsignal(int signo)
     if (signo > 0 && signo < NSIG && sudo_sys_siglist[signo] != NULL)
 	return (char *)sudo_sys_siglist[signo];
     /* XXX - should be "Unknown signal: %d" */
-    return _("Unknown signal");
+    return (char *)_("Unknown signal");
 }
 #endif /* HAVE_STRSIGNAL */
