8b898b2ca23d68409673bbfc9829b1c60a06f32a
Todd C. Miller <Todd.Miller@sudo.ws>
2022-11-09 12:58:41 -0700

    Test parsing LDIF when a backslash is the last char of the file. If run with address sanitizer, this test will crash when the fix in ceaf706ab74b is reverted.

---
+++ plugins/sudoers/regress/cvtsudoers/test39.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Test handling of a backslash at EOF with no trailing newline.
+#
+# If compiled with address sanitizer, cvtsudoers will crash without
+# the fix in ceaf706ab74b.
+#
+
+: ${CVTSUDOERS=cvtsudoers}
+
+printf 'dn: cn= Manager\\' | $CVTSUDOERS -c "" -b dc=sudo,dc=ws -i ldif -f sudoers
