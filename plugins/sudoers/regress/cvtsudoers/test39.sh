75008a0570f9dea3c9e5cbcbc319762adb6f9715
Todd C. Miller <Todd.Miller@sudo.ws>
2022-11-10 09:54:59 -0700

    Copy some LDIF test data from the cvtsudoers tests to the seed corpus. This includes a test to exercise the fix in PR #196.

--- plugins/sudoers/regress/cvtsudoers/test39.sh
+++ plugins/sudoers/regress/cvtsudoers/test39.sh
@@ -2,8 +2,8 @@
 #
 # Test handling of a backslash at EOF with no trailing newline.
 #
-# If compiled with address sanitizer, cvtsudoers will crash without
-# the fix in ceaf706ab74b.
+# If compiled with address sanitizer, cvtsudoers will crash without the
+# fix in ceaf706ab74b from https://github.com/sudo-project/sudo/pull/196.
 #
 
 : ${CVTSUDOERS=cvtsudoers}
