bd988685189dc31aae99de745d5a6ca066be63ba
Todd C. Miller <Todd.Miller@sudo.ws>
2022-12-29 15:44:33 -0700

    Exercise getter functions.

--- lib/util/regress/fuzz/fuzz_sudo_conf.c
+++ lib/util/regress/fuzz/fuzz_sudo_conf.c
@@ -26,6 +26,8 @@
 # include <inttypes.h>
 #endif
 
+#define SUDO_ERROR_WRAP 0
+
 #include "sudo_compat.h"
 #include "sudo_conf.h"
 #include "sudo_debug.h"
@@ -64,7 +66,13 @@ fuzz_conversation(int num_msgs, const struct sudo_conv_message msgs[],
 int
 LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
+    struct plugin_info_list *plugins = sudo_conf_plugins();
+    struct sudo_conf_debug_list *debug_list = sudo_conf_debugging();
+    struct sudo_conf_debug_file_list *debug_files;
     char tempfile[] = "/tmp/sudo_conf.XXXXXX";
+    struct sudo_conf_debug *debug_spec;
+    struct sudo_debug_file *debug_file;
+    struct plugin_info *info;
     size_t nwritten;
     int fd;
 
@@ -86,6 +94,52 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     /* sudo_conf_read() will re-init and free old data each time it runs. */
     sudo_conf_clear_paths();
     sudo_conf_read(tempfile, SUDO_CONF_ALL);
+
+    /* Path settings. */
+    if (sudo_conf_askpass_path() != NULL)
+	sudo_warnx("Path askpass %s", sudo_conf_askpass_path());
+    if (sudo_conf_sesh_path() != NULL)
+	sudo_warnx("Path sesh %s", sudo_conf_sesh_path());
+    if (sudo_conf_intercept_path() != NULL)
+	sudo_warnx("Path intercept %s", sudo_conf_intercept_path());
+    if (sudo_conf_noexec_path() != NULL)
+	sudo_warnx("Path noexec %s", sudo_conf_noexec_path());
+    if (sudo_conf_plugin_dir_path() != NULL)
+	sudo_warnx("Path plugin_dir %s", sudo_conf_plugin_dir_path());
+
+    /* Other settings. */
+    sudo_warnx("Set disable_coredump %s",
+	sudo_conf_disable_coredump() ? "true" : "false");
+    sudo_warnx("Set group_source %s",
+	sudo_conf_group_source() == GROUP_SOURCE_ADAPTIVE ? "adaptive" :
+	sudo_conf_group_source() == GROUP_SOURCE_STATIC ? "static" : "dynamic");
+    sudo_warnx("Set max_groups %d", sudo_conf_max_groups());
+    sudo_warnx("Set probe_interfaces %s",
+	sudo_conf_probe_interfaces() ? "true" : "false");
+
+    /* Plugins. */
+    plugins = sudo_conf_plugins();
+    TAILQ_FOREACH(info, plugins, entries) {
+	/* We don't bother with the plugin options. */
+	sudo_warnx("Plugin %s %s", info->symbol_name, info->path);
+    }
+
+    /* Debug settings. */
+    debug_list = sudo_conf_debugging();
+    TAILQ_FOREACH(debug_spec, debug_list, entries) {
+	TAILQ_FOREACH(debug_file, &debug_spec->debug_files, entries) {
+	    sudo_warnx("Debug %s %s %s", debug_spec->progname,
+		debug_file->debug_file, debug_file->debug_flags);
+	}
+    }
+
+    debug_files = sudo_conf_debug_files(getprogname());
+    if (debug_files != NULL) {
+	TAILQ_FOREACH(debug_file, debug_files, entries) {
+	    sudo_warnx("Debug %s %s %s", getprogname(),
+		debug_file->debug_file, debug_file->debug_flags);
+	}
+    }
 
     unlink(tempfile);
 
