sudo-project/sudo

commit fd4b369d7522e86479b2d30dfc4f75095d6c9ef5

sudoers_init: initialize ret to 0, not -1
This lets us more easily tell when sudoers has already been initialized
(or attempted to be initialized).  sudoers_init() returns 1 on
success and -1 on error (a 0 return value is never actually used).

Found by the ZeroPath AI Security Engineer <https://zeropath.com>

plugins/sudoers/sudoers.c
@@ -175,16 +175,20 @@ sudoers_reinit_defaults(struct sudoers_context *ctx)
     debug_return_bool(true);
 }
 
+/*
+ * Initialize sudoers data structures and parse sudoers sources.
+ * Returns 1 on success and -1 on error.
+ */
 int
 sudoers_init(void *info, sudoers_logger_t logger, char * const envp[])
 {
     struct sudo_nss *nss, *nss_next;
     int oldlocale, sources = 0;
-    static int ret = -1;
+    static int ret = 0;
     debug_decl(sudoers_init, SUDOERS_DEBUG_PLUGIN);
 
-    /* Only initialize once. */
-    if (snl != NULL)
+    /* Only initialize once, don't re-initialize on error (-1). */
+    if (ret != 0)
 	debug_return_int(ret);
 
     bindtextdomain("sudoers", LOCALEDIR);
@@ -197,29 +201,29 @@ sudoers_init(void *info, sudoers_logger_t logger, char * const envp[])
 
     /* Initialize environment functions (including replacements). */
     if (!env_init(envp))
-	debug_return_int(-1);
+	goto done;
 
     /* Setup defaults data structures. */
     if (!init_defaults()) {
 	sudo_warnx("%s", U_("unable to initialize sudoers default values"));
-	debug_return_int(-1);
+	goto done;
     }
 
     /* Parse info from front-end. */
     sudoers_ctx.mode = sudoers_policy_deserialize_info(&sudoers_ctx, info,
 	&initial_defaults);
     if (ISSET(sudoers_ctx.mode, MODE_ERROR))
-	debug_return_int(-1);
+	goto done;
 
     if (!init_vars(&sudoers_ctx, envp))
-	debug_return_int(-1);
+	goto done;
 
     /* Parse nsswitch.conf for sudoers order. */
     snl = sudo_read_nss();
 
     /* LDAP or NSS may modify the euid so we need to be root for the open. */
     if (!set_perms(NULL, PERM_ROOT))
-	debug_return_int(-1);
+	goto done;
 
     /* Use the C locale unless another is specified in sudoers. */
     sudoers_setlocale(SUDOERS_LOCALE_SUDOERS, &oldlocale);
@@ -257,7 +261,7 @@ sudoers_init(void *info, sudoers_logger_t logger, char * const envp[])
 
     /* Set login class if applicable (after sudoers is parsed). */
     if (set_loginclass(&sudoers_ctx))
-	ret = true;
+	ret = 1;
 
 cleanup:
     mail_parse_errors(&sudoers_ctx);
@@ -269,6 +273,9 @@ cleanup:
     sudo_warn_set_locale_func(NULL);
     sudoers_setlocale(oldlocale, NULL);
 
+done:
+    if (ret == 0)
+	ret = -1;
     debug_return_int(ret);
 }
 

plugins/sudoers/audit.c
@@ -181,7 +181,7 @@ sudoers_audit_open(unsigned int version, sudo_conv_t conversation,
     info.plugin_args = plugin_options;
     ret = sudoers_init(&info, log_parse_error, submit_envp);
 
-    if (ret == true) {
+    if (ret == 1) {
 	/* Unset close function if we don't need it to avoid extra process. */
 #ifdef SUDOERS_LOG_CLIENT
 	if (SLIST_EMPTY(&def_log_servers))

commit a579fcf05bf0fd34b1a4f9901d7ccc765b4ef51e

Add sudoers_initialized to determine if sudoers_init() was called.
This gets cleared in sudoers_cleanup(), making it it possible to
re-initialize sudoers after cleanup.  The policy fuzzer relies on
this behavior, broken by fd4b369d7522e86479b2d30dfc4f75095d6c9ef5.

plugins/sudoers/sudoers.c
@@ -175,6 +175,8 @@ sudoers_reinit_defaults(struct sudoers_context *ctx)
     debug_return_bool(true);
 }
 
+static bool sudoers_initialized;
+
 /*
  * Initialize sudoers data structures and parse sudoers sources.
  * Returns 1 on success and -1 on error.
@@ -184,12 +186,13 @@ sudoers_init(void *info, sudoers_logger_t logger, char * const envp[])
 {
     struct sudo_nss *nss, *nss_next;
     int oldlocale, sources = 0;
-    static int ret = 0;
+    int ret = -1;
     debug_decl(sudoers_init, SUDOERS_DEBUG_PLUGIN);
 
-    /* Only initialize once, don't re-initialize on error (-1). */
-    if (ret != 0)
+    /* Only attempt to initialize once. */
+    if (sudoers_initialized)
 	debug_return_int(ret);
+    sudoers_initialized = true;
 
     bindtextdomain("sudoers", LOCALEDIR);
 
@@ -201,29 +204,29 @@ sudoers_init(void *info, sudoers_logger_t logger, char * const envp[])
 
     /* Initialize environment functions (including replacements). */
     if (!env_init(envp))
-	goto done;
+	debug_return_int(-1);
 
     /* Setup defaults data structures. */
     if (!init_defaults()) {
 	sudo_warnx("%s", U_("unable to initialize sudoers default values"));
-	goto done;
+	debug_return_int(-1);
     }
 
     /* Parse info from front-end. */
     sudoers_ctx.mode = sudoers_policy_deserialize_info(&sudoers_ctx, info,
 	&initial_defaults);
     if (ISSET(sudoers_ctx.mode, MODE_ERROR))
-	goto done;
+	debug_return_int(-1);
 
     if (!init_vars(&sudoers_ctx, envp))
-	goto done;
+	debug_return_int(-1);
 
     /* Parse nsswitch.conf for sudoers order. */
     snl = sudo_read_nss();
 
     /* LDAP or NSS may modify the euid so we need to be root for the open. */
     if (!set_perms(NULL, PERM_ROOT))
-	goto done;
+	debug_return_int(-1);
 
     /* Use the C locale unless another is specified in sudoers. */
     sudoers_setlocale(SUDOERS_LOCALE_SUDOERS, &oldlocale);
@@ -261,7 +264,7 @@ sudoers_init(void *info, sudoers_logger_t logger, char * const envp[])
 
     /* Set login class if applicable (after sudoers is parsed). */
     if (set_loginclass(&sudoers_ctx))
-	ret = 1;
+	ret = true;
 
 cleanup:
     mail_parse_errors(&sudoers_ctx);
@@ -273,9 +276,6 @@ cleanup:
     sudo_warn_set_locale_func(NULL);
     sudoers_setlocale(oldlocale, NULL);
 
-done:
-    if (ret == 0)
-	ret = -1;
     debug_return_int(ret);
 }
 
@@ -1523,6 +1523,7 @@ sudoers_cleanup(void)
 	free(def->val);
 	free(def);
     }
+    sudoers_initialized = false;
     need_reinit = false;
     if (def_group_plugin)
 	group_plugin_unload();