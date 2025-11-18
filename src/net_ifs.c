sudo-project/sudo

commit 5846cdeda7d53c371971d940f00f4ce8f97ab45b

get_net_ifs: fix memory leak in SIOCGIFCONF version
Found by the ZeroPath AI Security Engineer <https://zeropath.com>

src/net_ifs.c
@@ -654,11 +654,10 @@ get_net_ifs(char **addrinfo_out)
     struct sockaddr_in6 *sin6;
 # endif
     char addrstr[INET6_ADDRSTRLEN], maskstr[INET6_ADDRSTRLEN];
-    char *addrinfo = NULL;
+    char *cp, addrinfo = NULL;
     int i, n, sock, sock4, sock6 = -1;
     int num_interfaces = 0;
     size_t ailen, buflen;
-    char *cp, *ifconf_buf = NULL;
     debug_decl(get_net_ifs, SUDO_DEBUG_NETIF);
 
     if (!sudo_conf_probe_interfaces())
@@ -859,7 +858,7 @@ bad:
     free(addrinfo);
     num_interfaces = -1;
 done:
-    free(ifconf_buf);
+    free(ifconf.ifc_buf);
     if (sock4 != -1)
         close(sock4);
     if (sock6 != -1)

commit 006513c88f133b9c8f3d7643728a92dcd8613075

get_net_ifs: fix typo in addrinfo declaration
This was a bug introduced in 5846cdeda7

src/net_ifs.c
@@ -654,7 +654,7 @@ get_net_ifs(char **addrinfo_out)
     struct sockaddr_in6 *sin6;
 # endif
     char addrstr[INET6_ADDRSTRLEN], maskstr[INET6_ADDRSTRLEN];
-    char *cp, addrinfo = NULL;
+    char *cp, *addrinfo = NULL;
     int i, n, sock, sock4, sock6 = -1;
     int num_interfaces = 0;
     size_t ailen, buflen;